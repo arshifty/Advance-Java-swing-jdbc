3d5a46ce3c4b972e3d0d647c6798be0f91a3a990
Ashif Rahman <38687585+arshifty@users.noreply.github.com>
2020-03-12 23:30:45 +0600

    Add files via upload

---
+++ higher_study_in_bangladesh/aboutClass.java
@@ -0,0 +1,15 @@
+
+
+package higher_study_in_bangladesh;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+
+public class aboutClass extends JFrame{
+    public aboutClass(JFrame frame)
+    {
+
+    }
+
+}
